a88697eae060f42e28194edc0dc99bc471add7c5
somesattysujana <somesattysujana@gmail.com>
2024-09-30 14:28:30 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+class student{
+public static void main(String[] agrs)[
+//code should be readable
+int maths=98
+int statistics=72
+int telugu=98
+int english=97
+int physics=89
+int sumofmarks=maths+statistics+telugu+english+physics;
+int average=sum of marks/6;
+system.out.printIn("sum of marks")
+system.out.printIn("average of marks")
+}
+}
